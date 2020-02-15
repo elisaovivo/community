5d15c8e6a3bb9a9e2be4a15596217af0077b1663
cj <cjbagend@gmail.com>
2020-02-15 11:51:52 +0800

    finish page list questions

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 [Bootstrap](https://v3.bootcss.com/getting-started/)
 [Github OAuth](https://developer.github.com/apps/building-oauth-apps/)
 [Spring](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
+[Thymeleaf](https://www.thymeleaf.org/doc/tutorials/3.0/usingthymeleaf.html#setting-attribute-values)
+[菜鸟教程](https://www.runoob.com/mysql/mysql-insert-query.html) 
 ## 工具
 [Git](https://git-scm.com/)
 [visual-paradigm](https://www.visual-paradigm.com)
--- src/main/java/life/cj/community/Controller/IndexController.java
+++ src/main/java/life/cj/community/Controller/IndexController.java
@@ -1,21 +1,30 @@
 package life.cj.community.Controller;
 
 
+import life.cj.community.dto.QuestionDTO;
+import life.cj.community.mapper.QuestionMapper;
 import life.cj.community.mapper.UserMapper;
+import life.cj.community.model.Question;
 import life.cj.community.model.User;
+import life.cj.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private QuestionService questionService;
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request,
+                        Model model){
         Cookie[] cookies = request.getCookies();
         if(cookies != null && cookies.length != 0)
             for (Cookie cookie : cookies) {
@@ -28,6 +37,9 @@ public class IndexController {
                     break;
                 }
             }
+
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
