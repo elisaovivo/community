2b26f7efa470d9334a4219ea8619bc594581810a
cj <cjbagend@gmail.com>
2020-04-20 12:09:27 +0800

    add interceptor

--- src/main/java/life/cj/community/Controller/ProfileController.java
+++ src/main/java/life/cj/community/Controller/ProfileController.java
@@ -17,8 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
-    @Autowired
-    private UserMapper userMapper;
+
     @Autowired
     private QuestionService questionService;
 
@@ -28,20 +27,8 @@ public class ProfileController {
                           Model model,
                           @RequestParam(name = "page", defaultValue = "1") Integer page,
                           @RequestParam(name = "size", defaultValue = "5") Integer size) {
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
+        User user = (User) request.getSession().getAttribute("user");
+
 
         if (user == null) {
             return "redirect:/";
